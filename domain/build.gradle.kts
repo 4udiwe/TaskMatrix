1af668d1ff1e677b2b99ac57e4650e8401674bc8
4udiwe <arsenijsarifkulov2633@gmail.com>
2024-10-25 15:33:27 +0300

    added testing in domain

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -7,7 +7,14 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
+tasks.test{
+    useJUnitPlatform()
+}
 
 dependencies{
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+
+    testImplementation(kotlin("test"))
+    testImplementation("org.mockito:mockito-core:4.0.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
 }
---
+++ domain/src/test/java/UseCaseTest.kt
@@ -0,0 +1,66 @@
+
+import com.example.domain.model.Task
+import com.example.domain.repository.TaskRepository
+import com.example.domain.usecase.AddTaskUseCase
+import com.example.domain.usecase.DeleteTaskUseCase
+import com.example.domain.usecase.GetAllTasksUseCase
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+
+class UseCaseTest {
+    private val repository = mock<TaskRepository>()
+    @AfterEach
+    fun tearDown(){
+        Mockito.reset(repository)
+    }
+    @Test
+    fun `should invoke add method`(){
+        val useCase = AddTaskUseCase(repository)
+
+        val testTask = Task(title = "testTask title")
+        runBlocking {
+            useCase.execute(newTask = testTask)
+            Mockito.verify(repository, times(1)).addTask(testTask)
+        }
+    }
+    @Test
+    fun `should invoke delete method`(){
+        val useCase = DeleteTaskUseCase(repository)
+
+        val testTask = Task(title = "testTask title")
+        runBlocking {
+            useCase.execute(task = testTask)
+            Mockito.verify(repository, times(1)).deleteTask(testTask)
+        }
+    }
+    @Test
+    fun `should receive all tasks`(){
+        val useCase = GetAllTasksUseCase(repository)
+
+        val expected = flowOf(
+            listOf(
+                Task(title = "testTask title 1"),
+                Task(title = "testTask title 2"),
+                Task(title = "testTask title 3"),
+                Task(title = "testTask title 4"),
+                Task(title = "testTask title 5")
+            )
+        )
+        Mockito.`when`(repository.getAllTasks()).thenReturn(expected)
+
+        val actual: Flow<List<Task>>
+
+        runBlocking {
+            actual = useCase.execute()
+        }
+        Assertions.assertEquals(expected, actual)
+        Mockito.verify(repository, times(1)).getAllTasks()
+    }
+}
